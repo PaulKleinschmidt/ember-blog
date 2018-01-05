5bcedba878c421d85a55d558eea599f5203237e0
paulkleinschmidt <paul.kleinschmidt1@gmail.com>
2018-01-05 16:57:36 -0500

    First commit containing the whole ember app

--- config/environment.js
+++ config/environment.js
@@ -1,51 +1,47 @@
-'use strict';
-
+/* jshint node: true */
 module.exports = function(environment) {
-  let ENV = {
-    modulePrefix: 'ember2-blog',
-    environment,
-    rootURL: '/',
-    locationType: 'auto',
-    EmberENV: {
-      FEATURES: {
-        // Here you can enable experimental features on an ember canary build
-        // e.g. 'with-controller': true
-      },
-      EXTEND_PROTOTYPES: {
-        // Prevent Ember Data from overriding Date.parse.
-        Date: false
-      }
-    },
-
-    APP: {
-      // Here you can pass flags/options to your application instance
-      // when it is created
-    }
-  };
-
-  if (environment === 'development') {
-    // ENV.APP.LOG_RESOLVER = true;
-    // ENV.APP.LOG_ACTIVE_GENERATION = true;
-    // ENV.APP.LOG_TRANSITIONS = true;
-    // ENV.APP.LOG_TRANSITIONS_INTERNAL = true;
-    // ENV.APP.LOG_VIEW_LOOKUPS = true;
-  }
-
-  if (environment === 'test') {
-    // Testem prefers this...
-    ENV.locationType = 'none';
-
-    // keep test console output quieter
-    ENV.APP.LOG_ACTIVE_GENERATION = false;
-    ENV.APP.LOG_VIEW_LOOKUPS = false;
-
-    ENV.APP.rootElement = '#ember-testing';
-    ENV.APP.autoboot = false;
-  }
-
-  if (environment === 'production') {
-    // here you can enable a production-specific feature
-  }
-
-  return ENV;
+ var ENV = {
+ modulePrefix: 'ember2-blog',
+ environment: environment,
+ contentSecurityPolicy: { 'connect-src': "'self' wss://*.firebaseio.com" },
+ firebase: {
+   apiKey: "AIzaSyDXEvrgB6l_iX2ICOHgqWbsSsVl48YP5NQ",
+   authDomain: "ember-blog2-e5aea.firebaseapp.com",
+   databaseURL: "https://ember-blog2-e5aea.firebaseio.com",
+   projectId: "ember-blog2-e5aea",
+   storageBucket: "ember-blog2-e5aea.appspot.com",
+   messagingSenderId: "20925597941"
+  },
+ baseURL: '/',
+ locationType: 'auto',
+ EmberENV: {
+ FEATURES: {
+ // Here you can enable experimental features on an ember canary build
+ // e.g. ‘with-controller’: true
+ }
+ },
+APP: {
+ // Here you can pass flags/options to your application instance
+ // when it is created
+ }
+ };
+if (environment === 'development') {
+ // ENV.APP.LOG_RESOLVER = true;
+ // ENV.APP.LOG_ACTIVE_GENERATION = true;
+ // ENV.APP.LOG_TRANSITIONS = true;
+ // ENV.APP.LOG_TRANSITIONS_INTERNAL = true;
+ // ENV.APP.LOG_VIEW_LOOKUPS = true;
+ }
+if (environment === 'test') {
+ // Testem prefers this…
+ ENV.baseURL = '/';
+ ENV.locationType = 'none';
+// keep test console output quieter
+ ENV.APP.LOG_ACTIVE_GENERATION = false;
+ ENV.APP.LOG_VIEW_LOOKUPS = false;
+ENV.APP.rootElement = '#ember-testing';
+ }
+if (environment === 'production') {
+}
+return ENV;
 };
